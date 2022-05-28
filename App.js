c541b1032f7cc3c273fc1f271c3f68228d7a7f8c
Florsk <davi.galdino08@gmail.com>
2022-05-27 21:18:24 -0300

    Primeiras aulas - v1.1

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { View, SafeAreaView, Text } from 'react-native';
+import { View, SafeAreaView, Text, StyleSheet } from 'react-native';
+import Params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component{
   render() {
     return(
-      <View>
-        <Text>Testanto</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da grade: {Params.getRowsAmaout()}x{Params.getColumsAmout}</Text>
+
+        <Field />
+        <Field opened/>
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={6}/>
+        <Field opened nearMines={8}/>
+        <Field opened nearMines={5}/>
+
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF'
+  },
+  welcome:{
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+})
